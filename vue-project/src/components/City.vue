570e3e2d4d1774881b8c86af7079df2f8dbfd6b3
syl566 <sylvie.bal@le-campus-numerique.fr>
2026-04-30 13:53:51 +0200

    composant date

--- vue-project/src/components/City.vue
+++ vue-project/src/components/City.vue
@@ -1,11 +1,31 @@
 <template>
-  <div>
+  <div class="card">
     <h1>Météo - Détail d'une ville</h1>
+
+    <h2>{{ city.name }}</h2>
+    <p>{{ city.weather }}</p>
+    <p>{{ city.temperature }} °C</p>
+
+    <p>mise à jour le {{ formatDate(city.updatedAt) }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'City'
+  data() {
+    return {
+      city: {
+        name: 'Ma ville',
+        weather: 'Peu nuageux',
+        temperature: 20.55,
+        updatedAt: new Date()
+      }
+    }
+  },
+  methods: {
+    formatDate(date) {
+      return new Date(date).toLocaleString()
+    }
+  }
 }
 </script>
